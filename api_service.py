604f868c5febd531bdda3c8f791403a6e0f0d6de
unknown <kevinwang312@gmail.com>
2023-03-20 17:55:02 -0400

    added option to input number of colors

--- api_service.py
+++ api_service.py
@@ -5,10 +5,11 @@ from visual import display_color_palette
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 prompt = input("What would you like a color palette for?\n")
+number = input("How many colors would you like?\n")
 
 response = openai.Completion.create(
     model="text-davinci-003",
-    prompt="Recommend a color pallet for "+prompt+". Return only hex values in the from of an string array.",
+    prompt="Recommend a color pallet with "+number+" colors for "+prompt+". Return only hex values in the from of an string array.",
     temperature=0.75,
     max_tokens=70,
     top_p=1.0,
